dea7747e40222707fc0edbb29144793086ce7721
Danuka13 <156576225+Danuka13@users.noreply.github.com>
2024-10-15 19:21:57 +0530

    Update UpdateProduct.js

--- frontend/src/components/UpdateProduct.js
+++ frontend/src/components/UpdateProduct.js
@@ -27,7 +27,8 @@ export default function InsertProduct() {
     useEffect(() => {
         const getProduct = async () => {
           try {
-            const res = await fetch(`http://localhost:3001/products/${id}`, {
+            const res = await fetch(`des-final-danuka13s-projects.vercel.app
+/products/${id}`, {
               method: "GET",
               headers: {
                 "Content-Type": "application/json"
@@ -64,7 +65,8 @@ export default function InsertProduct() {
         setError("");
 
         try {
-            const response = await fetch(`http://localhost:3001/updateproduct/${id}`, {
+            const response = await fetch(`des-final-danuka13s-projects.vercel.app
+/updateproduct/${id}`, {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json"
